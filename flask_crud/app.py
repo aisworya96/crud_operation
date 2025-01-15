555aba84c67f81606a72775f04f0df48e9626042
apple <paisworya2@gmail.com>
2025-01-15 15:03:09 +0530

    Added PUT operation to crud functionality

--- flask_crud/app.py
+++ flask_crud/app.py
@@ -22,5 +22,15 @@ def create_item():
     return jsonify(item), 201
 
 
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    data = request.get_json()
+    item = next((item for item in items if item['id'] == item_id), None)
+    if item is None:
+        return jsonify({'error': 'Item not found'}), 404
+    item['name'] = data.get('name', item['name'])
+    return jsonify(item), 200
+
+
 if __name__ == '__main__':
     app.run()
