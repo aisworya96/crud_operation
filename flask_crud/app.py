5bcc549528c3eaaa072b1211f5805dc32944d18d
apple <paisworya2@gmail.com>
2025-01-15 10:08:36 +0530

    Updated the file and added CRUD operation to app.py

--- flask_crud/app.py
+++ flask_crud/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
-from flaskext.mysql import MySQL
-import pymysql
-
-mysql = MySQL()
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+items = []
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/items', methods = ['POST'])
+def create_item():
+    data = request.get_json()
+    item = {
+        'id': len(items) + 1,
+        'name': data.get('name')
+
+    }
+    items.append(item)
+    return jsonify(item), 201
+
 
 if __name__ == '__main__':
     app.run()
